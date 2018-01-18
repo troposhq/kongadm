b2c7eb4ae3384c717d239ebe076ac0b587a7db39
Alois Barreras <aloisbarreras@icloud.com>
2018-01-18 16:56:58 -0500

    tracking file

---
+++ api/consumer.go
@@ -0,0 +1,21 @@
+package api
+
+// ListConsumersResult is the API response from Kong for fetching Consumers
+type ListConsumersResult struct {
+	Total int        `json:"total"`
+	Data  []Consumer `json:"data"`
+}
+
+// Consumer is a Kong Consumer object
+type Consumer struct {
+	ID        string `json:"id"`
+	CustomID  string `json:"custom_id"`
+	CreatedAt int    `json:"created_at"`
+}
+
+// ListConsumers fetches Consumers from the Kong API
+func ListConsumers() (r ListConsumersResult, err error) {
+	req := buildRequest("GET", apiURLBase+"consumers", nil, nil)
+	err = makeRequest(req, &r)
+	return r, err
+}
